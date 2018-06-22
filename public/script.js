91a72c6568a0228c6844a5b847f52b50a1b8a94e
Nicholas Kline <ndkline@theagora.us>
2018-06-22 16:54:27 -0400

    @ Section 4 | Lecture 23
    https://www.udemy.com/vuejs-2-essentials/learn/v4/t/lecture/6197442?start=0

--- public/script.js
+++ public/script.js
@@ -1,1 +1,22 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            { title: 'Item 1', qty: 0 },
+            { title: 'Item 2', qty: 0 },
+            { title: 'Item 3', qty: 0 }
+        ],
+        cart: []
+    },
+    methods:{
+        addItem: function(index){
+            this.total += 9.99;
+            var item = this.items[index];
+            this.cart.push({
+                title: item.title,
+                qty: 1
+            });
+        }
+    }
+});
